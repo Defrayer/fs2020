e36776df64946ef56b128bb2ee25956e5763a03e
jupekett <jupekett@student.jyu.fi>
2020-08-14 11:47:23 +0300

    Poista string literal -kikkailu

--- osa1/unicafe/src/components/Display.js
+++ osa1/unicafe/src/components/Display.js
@@ -5,7 +5,11 @@ import React from 'react';
 const Display = (props) => {
   const { text, value, unit } = props;
 
-  return <p>{`${text}: ${value} ${unit || ''}`}</p>;
+  return (
+    <p>
+      {text}: {value} {unit || ''}
+    </p>
+  );
 };
 
 export default Display;
