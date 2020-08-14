9dac73b2fd44ac8ff1dde5963009906f91eec627
jupekett <jupekett@student.jyu.fi>
2020-08-14 15:13:00 +0300

    Anekdootit step 2
    Toteuta äänien antaminen anekdooteille.

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -9,13 +9,15 @@ import anecdotes from './data/anecdotes.js';
  */
 const App = (props) => {
   const [selected, setSelected] = useState(0);
+  const initialVotes = anecdotes.map((anecdote) => 0);
+  const [votes, setVotes] = useState(initialVotes);
 
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
   };
 
-  const handleClick = () => {
+  const handleNext = () => {
     while (true) {
       const newSelected = getRandomInt(anecdotes.length);
       if (newSelected !== selected) {
@@ -25,12 +27,20 @@ const App = (props) => {
     }
   };
 
+  const handleVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
   return (
     <div>
       <h1>Anecdote of the day</h1>
       {props.anecdotes[selected]}
+      <p>This anecdote has {votes[selected]} votes.</p>
       <p>
-        <button onClick={handleClick}>Next anecdote</button>
+        <button onClick={handleVote}>Vote</button>
+        <button onClick={handleNext}>Next anecdote</button>
       </p>
     </div>
   );
