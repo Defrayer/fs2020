f07684216b97d36c5b41916ec3e5438a789ccf11
jupekett <jupekett@student.jyu.fi>
2020-08-19 17:14:18 +0300

    2.4-5: Kurssitiedot steps 9 & 10
    Toteuta kurssien piirtäminen mapilla.
    Courses-komponentti omaksi moduulikseen (ja muutkin olivat jo
    valmiiksi).

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -3,29 +3,54 @@
 import React from 'react';
 import Course from './components/Course';
 
+/**
+ * @author: Juho Kettunen jupekett
+ */
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-      { name: 'JavaScript: The Imaginary Parts', exercises: -3 },
-    ],
-  };
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+        },
+        { name: 'JavaScript, the imaginary parts', exercises: -3 },
+      ],
+    },
+
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   );
 };
--- osa2/kurssitiedot/src/components/Total.js
+++ osa2/kurssitiedot/src/components/Total.js
@@ -4,8 +4,8 @@ import React from 'react';
 
 const Total = (props) => (
   <p style={{ fontWeight: 600 }}>
-    Total of {props.parts.reduce((total, part) => total + part.exercises, 0)}{' '}
-    exercises.
+    Number of exercises:{' '}
+    {props.parts.reduce((total, part) => total + part.exercises, 0)}
   </p>
 );
 
