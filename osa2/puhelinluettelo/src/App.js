e0ad8608b5d387f7ff311aad69667ee5ebbfe83c
jupekett <jupekett@student.jyu.fi>
2020-08-19 21:40:16 +0300

    2.8: puhelinluettelo step3
    Lisää puhelinnumeron käsittely lomakkeeseen.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,26 +6,38 @@ import React, { useState } from 'react';
  * @author Juho Kettunen jupekett
  */
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Erkki Esimerkki' }]);
-  const [newName, setNewName] = useState('');
+  const [persons, setPersons] = useState([
+    { name: 'Erkki Esimerkki', number: '123-4567890' },
+  ]);
 
-  // Lisää henkilön puhelinluetteloon. Ei hyväksy duplikaattinimiä.
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
+  // Lisää henkilön puhelinluetteloon. Ei hyväksy duplikaattinimiä tai tyhjää.
   const handleSubmit = (event) => {
     event.preventDefault();
-    const name = newName.trim().toLowerCase();
+    if (!newName.trim()) {
+      alert('Name cannot be blank.');
+      setNewName('');
+      return;
+    }
+    if (!newNumber.trim()) {
+      alert('Number cannot be blank.');
+      setNewNumber('');
+      return;
+    }
     const nameIsInPhonebook = persons.some(
-      (person) => person.name.trim().toLowerCase() === name
+      (person) =>
+        person.name.trim().toLowerCase() === newName.trim().toLowerCase()
     );
     if (nameIsInPhonebook) {
       alert(`${newName} is already added to the phonebook.`);
     } else {
-      setPersons(persons.concat({ name: newName.trim() }));
+      const newPerson = { name: newName.trim(), number: newNumber.trim() };
+      setPersons(persons.concat(newPerson));
       setNewName('');
+      setNewNumber('');
     }
-  };
-
-  const handleChange = (event) => {
-    setNewName(event.target.value);
   };
 
   return (
@@ -33,15 +45,30 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          <label htmlFor="nameField">Name: </label>
+          <input
+            id="nameField"
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            required
+          />
         </div>
         <div>
-          <button type="submit">Add</button>
+          <label htmlFor="numberField">Number: </label>
+          <input
+            id="numberField"
+            type="tel"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+            required
+          />
         </div>
+        <button type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name + ' ' + person.number}</p>
       ))}
     </div>
   );
