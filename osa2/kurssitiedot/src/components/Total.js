8aad07b9baa6053c899b9569683ecd90c582279b
jupekett <jupekett@student.jyu.fi>
2020-08-19 17:00:41 +0300

    2.3-2.4: kurssitiedot steps 8 & 9
    Lisää kurssien tehtävien kokonaislukumäärän laskeminen reducella.

--- osa2/kurssitiedot/src/components/Total.js
+++ osa2/kurssitiedot/src/components/Total.js
@@ -3,9 +3,9 @@
 import React from 'react';
 
 const Total = (props) => (
-  <p>
-    Number of exercises{' '}
-    {props.parts.reduce((total, part) => total + part.exercises, 0)}
+  <p style={{ fontWeight: 600 }}>
+    Total of {props.parts.reduce((total, part) => total + part.exercises, 0)}{' '}
+    exercises.
   </p>
 );
 
