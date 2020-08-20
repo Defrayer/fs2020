fe83ada60bc001e9af1fe00c682247997b65af2c
jupekett <jupekett@student.jyu.fi>
2020-08-20 19:44:02 +0300

    Ensimmäinen commit

---
+++ osa2/maiden_tiedot/README.md
@@ -0,0 +1,3 @@
+# FS2020 osa 2: maiden tiedot
+
+Tehtävät 2.12-2.14
---
+++ osa2/maiden_tiedot/src/App.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return <p>asd</p>;
+}
+
+export default App;
